830a7351cc00ac4624cc31a46187464845edc141
szbgreg <szbgreg@gmail.com>
2020-12-14 11:30:10 +0100

    Adding Header component

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ class IndecisionApp extends React.Component {
   render() {
     return (
       <div>
-        <div>
-          <h1>Indecision App</h1>
-          <h2>Put your life in hands of the computer</h2>
-        </div>
+        <Header />
         <div>
           <button>What should I do?</button>
         </div>
+      </div>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision App</h1>
+        <h2>Put your life in hands of the computer</h2>
       </div>
     );
   }
