f2796d4814d86d5a9aff1334e99ecfa8152088a3
szbgreg <szbgreg@gmail.com>
2020-12-15 15:17:20 +0100

    Adding webpack-dev-server and change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,35 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+module.exports = (env) => {
+  isProduction = env === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: ['@babel/polyfill', './src/app.js'],
+    output: {
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
           }
         }
-      }
-    ]
-  }
+      ]
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      port: 8080,
+      historyApiFallback: true,
+      publicPath: '/dist/'
+    }
+  };
 };
